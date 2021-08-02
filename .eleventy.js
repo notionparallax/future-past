17a85edc2986ea0806cd447c279a598ccc2f3ba9
Ben <ben@notionparallax.co.uk>
2021-08-02 18:26:09 +1000

    add a filter to sort by week

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,14 @@
 module.exports = function (eleventyConfig) {
+    eleventyConfig.addFilter("sortByWeek", function (values) {
+        let vals = [...values]; // this *seems* to prevent collection mutation...
+
+        return [...vals.sort((a, b) => Math.sign(a.data.week - b.data.week))];
+    });
+
+    eleventyConfig.addFilter("dumdum", function () {
+        return "ballbag";
+    });
+
     return {
         dir: {
             // ⚠️ These values are both relative to your input directory.
